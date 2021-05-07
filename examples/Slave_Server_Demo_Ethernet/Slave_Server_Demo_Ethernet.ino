45319806011e87aee91be24f39bb30ce7f60d813
Agung Julian Perkasa <agungjulianperkasa@gmail.com>
2021-05-07 15:02:21 +0700

    translate from italian to english

--- examples/Slave_Server_Demo_Ethernet/Slave_Server_Demo_Ethernet.ino
+++ examples/Slave_Server_Demo_Ethernet/Slave_Server_Demo_Ethernet.ino
@@ -3,8 +3,8 @@
 #include <Ethernet.h>
 #include <IEC60870-5-104.h>
 
-//Creo l'istanza SERVER IEC60870-5-104
-EthernetServer srv(2404); //Creo il server
+//I create the IEC60870-5-104 SERVER instance
+EthernetServer srv(2404); //I create the server
 IEC104_SLAVE slave(&srv);
 
 //MAC address
@@ -13,77 +13,77 @@ byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 //Set the static IP address to use if the DHCP fails to assign
 IPAddress ip(192, 168, 2, 177);
 
-//Memoria per timer
+//Memory per timer
 unsigned long lastSend=0;
 
-//Setup - viene eseguito all'avvio
+//Setup - runs at startup
 void setup()
 {
-  //Imposto la comunicazione seriale
+  //I set up serial communication
   Serial.begin(115200);
 
-  //Imposto la scheda di rete
+  //I set up the network card
   Ethernet.init(5);   // MKR ETH shield
   Ethernet.begin(mac, ip);
 }
 
-//Loop - Ciclo infinito su CORE 1 
+//Loop - Infinite loop on CORE 1
 void loop()
 {
-  //------------------------------------------------------------RICEZIONE DI UN COMANDO SP o DP------------------------------------------------------------//
+  //------------------------------------------------------------RECEIVING AN SP or DP COMMAND------------------------------------------------------------//
   
-  //Con la funzione available aggiorno il server e ottengo in uscita il numero di telegrammi in attesa di essere letti
+  //With the function available, I update the server and obtain the number of telegrams waiting to be read in output
   int messages = slave.available();
 
-  //Eseguo un ciclo for per leggere tutti i messaggi nel buffer
+  //I run a for loop to read all the messages in the buffer
   for(int i=0; i<messages; i++)
   {
-    //Creo delle variabili di appoggio per leggere il buffer
+    //I create support variables to read the buffer
     byte type = 0; int ca = 0; long ioa = 0; long value = 0;
 
-    //Eseguo la lettura di un array nel buffer
+    //I read an array in the buffer
     slave.read(&type,&ca,&ioa,&value);
 
-    //Utilizzo il dato ricevuto
+    //I use the data received
     if(type==C_SC_NA_1) Serial.println("You received Single Point Command from C.A.:"+String(ca)+"-IOA:"+String(ioa)+" with value "+String(value));
     else if(type==C_DC_NA_1) Serial.println("You received Double Point Command from C.A.:"+String(ca)+"-IOA:"+String(ioa)+" with value "+String(value));
   }
 
-  //---------------------------------------------------------------INVIO DI UNA MISURA SP o ME---------------------------------------------------------------//
+  //---------------------------------------------------------------SENDING A SP or ME MEASUREMENT---------------------------------------------------------------//
 
     //Scelgo il mio Common Address
     int ca = 50;
 
-    //Creo quattro variabili da inviare, due booleane (Single Point), e due analogiche (Normalized Measure)
+    //I create four variables to send, two Boolean (Single Point), and two analog (Normalized Measure)
     bool val1 = false;
     bool val2 = true;
     int val3 = 500;
     int val4 = -500;
 
-    //Scalo i valori per renderli normalizzati
+    //I scale the values to make them normalized
     long val3n = map(500,0,1000,0,32767); 
     long val4n = map(-500,-1000,1000,-32767,32767);
 
-    //Posso inviare più oggetti dello stesso tipo in un unico telegramma
+    //I can send several objects of the same type in one telegram
     
-    long IOA_SP[2] = {10, 11}; //Inserisco gli IOA che userò per i single point
-    long IOA_ME[2] = {20, 21}; //Inserisco gli IOA che userò per le misure
+    long IOA_SP[2] = {10, 11}; //I enter the IOAs that I will use for single points
+    long IOA_ME[2] = {20, 21}; //I enter the IOAs that I will use for the measurements
     
-    long VAL_SP[2] = {val1, val2}; //Inserisco i valori
-    long VAL_ME[2] = {val3n, val4n}; //Inserisco i valori
+    long VAL_SP[2] = {val1, val2}; //I enter the values
+    long VAL_ME[2] = {val3n, val4n}; //I enter the values
 
-    //Se sono trascorsi 5" dall'ultimo invio, reinvio la misura
+    //If 5 "have passed since the last sending, resend the measurement
     if(millis()-lastSend>5000)
     {
-      //Eseguo l'invio dei Measured Normalized Value
-      int quantitySP = 2; //Numero di oggetti da inviare
+      //I send the Measured Normalized Value
+      int quantitySP = 2; //Number of items to send
       slave.send(M_ME_NA_1,quantitySP,ca,IOA_SP,VAL_SP);
 
-      //Eseguo l'invio dei Single Point Information
-      int quantityME = 2; //Numero di oggetti da inviare
+      //I send the Single Point Information
+      int quantityME = 2; //Number of items to send
       slave.send(M_SP_NA_1,quantityME,ca,IOA_ME,VAL_ME);
 
-      //Aggiorno la memoria
+      //I update the memory
       lastSend=millis();
     }
 }
